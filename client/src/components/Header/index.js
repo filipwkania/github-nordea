5788851be74def402dc8b90e3eb15886f4550850
filipwkania <livarion@gmail.com>
2018-05-25 19:56:16 +0200

    add reactstrap as a component library for our app

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+const Header = () => (
+  <header className="app-header">
+    <Alert color="success">
+      Hello!
+    </Alert>
+  </header>
+);
+
+export default Header;
