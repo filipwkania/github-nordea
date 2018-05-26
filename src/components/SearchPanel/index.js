a91f695cff6a1cbc8f81c857576cb331495fac0a
filipwkania <livarion@gmail.com>
2018-05-26 22:06:07 +0200

    responsive header

--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import debounce from 'lodash.debounce';
 import request from 'axios/index';
-import { Search, Dropdown, Item, Image, Container, Popup } from 'semantic-ui-react';
+import { Search, Dropdown, Item, Image, Container, Popup, Responsive } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 
 const accessToken = process.env.REACT_APP_GITHUB_TOKEN;
@@ -57,20 +57,22 @@ class SearchPanel extends React.Component {
 
     return (
       <Container>
-        <Popup
-          trigger={
-            <Item data-content="">
-              <a
-                href="https://www.linkedin.com/in/filip-kania-876a5095/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image avatar src="https://avatars1.githubusercontent.com/u/1859589?s=100&u=2d4be0f7b0207dcd0c9f5f17ed20a1027c9e27e5&v=4" />
-              </a>
-            </Item>
-          }
-          content="Visit my linked in"
-        />
+        <Responsive minWidth={768}>
+          <Popup
+            trigger={
+              <Item data-content="">
+                <a
+                  href="https://www.linkedin.com/in/filip-kania-876a5095/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image avatar src="https://avatars1.githubusercontent.com/u/1859589?s=100&u=2d4be0f7b0207dcd0c9f5f17ed20a1027c9e27e5&v=4" />
+                </a>
+              </Item>
+            }
+            content="Visit my LinkedIn"
+          />
+        </Responsive>
         <Search
           className="search-panel-input item"
           value={searchPhrase}
@@ -82,12 +84,12 @@ class SearchPanel extends React.Component {
           style={{ flexGrow: 1 }}
           placeholder="Type github username..."
         />
-        <Item>
+        <Responsive as={Item} minWidth={768}>
           <Dropdown
             placeholder="Recent queries"
             options={[{ text: 'Not implemented yet :)' }]}
           />
-        </Item>
+        </Responsive>
       </Container>
     );
   }
