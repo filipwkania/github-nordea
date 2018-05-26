dfb3eaba9a5cb31112ff19013ed94e33062e04b7
filipwkania <livarion@gmail.com>
2018-05-26 17:56:54 +0200

    wrap result items in segments

--- src/components/ResultRow/index.js
+++ src/components/ResultRow/index.js
@@ -1,48 +1,49 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { Grid, Item, Label } from 'semantic-ui-react';
+import { Grid, Item, Label, Segment } from 'semantic-ui-react';
 
 const ResultRow = ({ repo }) => (
   <Grid.Column mobile={16} computer={8}>
-    <Item>
-      <Item.Content>
-        <Item.Header
-          className="ui header"
-          content={repo.name}
-        />
-        <Item.Meta className="small">
+    <Segment>
+      <Item>
+        <Item.Content>
+          <Item.Header
+            className="ui header"
+            content={repo.name}
+          />
+          <Item.Meta className="small">
            updated at {new Date(repo.updated_at).toLocaleDateString()}
-        </Item.Meta>
-        <Item.Description content={repo.description} />
-        <Item.Extra>
-          {
-            repo.language
-            &&
-            <Label
-              icon="code"
-              content={repo.language}
-            />
-          }
-          {
-            repo.stargazers_count > 0
-            &&
-            <Label
-              icon="star"
-              content={repo.stargazers_count}
-            />
-          }
-
-          {
-            repo.forks_count > 0
-            &&
-            <Label
-              icon="fork"
-              content={repo.forks_count}
-            />
-          }
-        </Item.Extra>
-      </Item.Content>
-    </Item>
+          </Item.Meta>
+          <Item.Description content={repo.description} />
+          <Item.Extra>
+            {
+              repo.language
+                &&
+                <Label
+                  icon="code"
+                  content={repo.language}
+                />
+            }
+            {
+              repo.stargazers_count > 0
+                &&
+                <Label
+                  icon="star"
+                  content={repo.stargazers_count}
+                />
+            }
+            {
+              repo.forks_count > 0
+                &&
+                <Label
+                  icon="fork"
+                  content={repo.forks_count}
+                />
+            }
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </Segment>
   </Grid.Column>
 );
 
