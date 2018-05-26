4b2bae22ffa59a42ce75a0883770ae1547d1c875
filipwkania <livarion@gmail.com>
2018-05-26 10:59:34 +0200

    server structure change

--- src/db/config.js
+++
@@ -1,5 +0,0 @@
-// Insert mongo url here
-// For more, see: https://docs.mongodb.com/manual/reference/connection-string/
-const url = 'mongodb://localhost:27017';
-
-module.exports.url = url;
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.send('hello world'));
-router.get('/test', (req, res) => res.send('hello test'));
-
-module.exports = router;
