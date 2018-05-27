e67a5c1e4f80b0042f4946297bf10c5e12b6b572
filipwkania <livarion@gmail.com>
2018-05-27 21:47:46 +0200

    footer styling, add buttons to linked and github, also: spaceman

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
-import { Item } from 'semantic-ui-react';
+import { Item, Button, Grid } from 'semantic-ui-react';
 
 const Footer = () => (
-  <footer className="app-footer ui vertical footer">
+  <footer
+    className="app-footer ui vertical footer inverted"
+
+  >
+    <a
+      href="https://www.linkedin.com/in/filip-kania-876a5095/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button
+        content="LinkedIn"
+        color="linkedin"
+        icon="linkedin"
+        size="tiny"
+      />
+    </a>
     <Item
-      content="&copy; Filip Kania 2018"
       style={{
-        padding: '15px 0',
         textAlign: 'center',
+        margin: '0 15px',
       }}
-    />
+    >
+      <Item.Content verticalAlign="middle">
+              &copy; Filip Kania 2018
+      </Item.Content>
+    </Item>
+    <a
+      href="https://github.com/filipwkania"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Button
+        content="GitHub"
+        color="grey"
+        icon="github"
+        size="tiny"
+      />
+    </a>
   </footer>
 );
 
