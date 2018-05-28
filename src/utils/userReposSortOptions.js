bb4f3eac965d3e5f89d6039806522ad751e96e4e
filipwkania <livarion@gmail.com>
2018-05-29 01:47:39 +0200

    move sort options to separate file

---
+++ src/utils/userReposSortOptions.js
@@ -0,0 +1,16 @@
+const sortByOptions = [
+  {
+    text: 'Last created',
+    value: '&sort=created',
+  },
+  {
+    text: 'Last updated',
+    value: '&sort=updated',
+  },
+  {
+    text: 'Default',
+    value: '',
+  },
+];
+
+export default sortByOptions;
