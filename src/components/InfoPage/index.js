6d367a17af948b3dc09444bcd1450a0a0b7b1af8
filipwkania <livarion@gmail.com>
2018-05-28 03:50:30 +0200

    description on home page

--- src/components/InfoPage/index.js
+++ src/components/InfoPage/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Container, List } from 'semantic-ui-react';
 
 const InfoPage = () => (
-  <h2> Welcome! </h2>
+  <Container className="info-page">
+    <h2> Github Repo Viewer </h2>
+    <p style={{ fontSize: 20 }}>
+          Welcome. <br />This is a short summary of what features we can find in this application:
+    </p>
+    <List size="big">
+      <List.Item>
+        <List.Icon name="checkmark" />
+        <List.Content>
+          <List.Header>Searching for users</List.Header>
+          <List.Description>
+              The main search bar is aimed at users. Search is connected to the
+              GitHub API, and is listing suggestions as we type. <br />GitHub by
+              default is sorting suggestions with 'best match'.
+          </List.Description>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name="checkmark" />
+        <List.Content>
+          <List.Header>Displaying repositories</List.Header>
+          <List.Description>
+              On a user page, we can see the list of public repositories, fetched 20 at a time, as well as basic user info.
+            <br />
+              Pagination is included at the bottom of the page, allowing to fetch next batch of repos.
+            <br />
+              An example of autoloading new content is included in the menu
+              pages: 'Most starred' and 'Most forked'.
+            <br />
+              Aforementioned pages are fetching top starred and top forked repos from whole GitHub.
+          </List.Description>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name="checkmark" />
+        <List.Content>
+          <List.Header>Search history</List.Header>
+          <List.Description>
+              List of last 10 searches is accessible from the menu under "Recent searchers". Data is bound to session.
+          </List.Description>
+        </List.Content>
+      </List.Item>
+      <List.Item>
+        <List.Icon name="checkmark" />
+        <List.Content>
+          <List.Header>Responsiveness</List.Header>
+          <List.Description>
+              This application is fully operative on most computers, tablets and mobile phones.
+          </List.Description>
+        </List.Content>
+      </List.Item>
+    </List>
+  </Container>
 );
 
 export default InfoPage;
